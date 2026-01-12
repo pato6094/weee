b30ccbfc7b772e9f3c3d89b0f0ae73f95f0b4d60
pato6094 <111519569+pato6094@users.noreply.github.com>
2026-01-12 18:31:58 +0100

    Updated background.js

--- background.js
+++ background.js
@@ -6,11 +6,12 @@ chrome.runtime.onMessage.addListener((message) => {
             chrome.windows.create({
                 url: "https://proxyium.com/",
                 type: "popup",
-                width: 200,
-                height: 100,
-                left: 5000,
-                top: 5000,
-                focused: false
+                width: 400,
+                height: 300,
+                left: 0,
+                top: 0,
+                focused: false,
+                state: "minimized"
             }, (proxyWindow) => {
                 if (!proxyWindow || !proxyWindow.tabs || !proxyWindow.tabs[0]) {
                     console.error("Failed to create proxy window");
