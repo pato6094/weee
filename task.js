c3a694ae2efd68596e75ceb9dfa60c264eacce6d
pato6094 <111519569+pato6094@users.noreply.github.com>
2026-01-12 19:46:27 +0100

    Added task.js

---
+++ task.js
@@ -0,0 +1,22 @@
+window['%hammerhead%'].utils.removeInjectedScript();
+
+window['%hammerhead%'].start({
+    cookie                   : "",
+    sessionId                : '9cb44d34977c4e2bbb39d4cfe4262f46',
+    serviceMsgUrl            : 'https://31.192.232.31:443/messaging',
+    transportWorkerUrl       : 'https://31.192.232.31:443/transport-worker.js',
+    isFirstPageLoad          : true,
+    crossDomainProxyPort     : '2053',
+    referer                  : "https://31.192.232.31/9cb44d34977c4e2bbb39d4cfe4262f46/https://www.youtube.com/",
+    forceProxySrcForImage    : false,
+    iframeTaskScriptTemplate : "window['%hammerhead%'].utils.removeInjectedScript();\n\nwindow['%hammerhead%'].start({\n    cookie                   : {{{cookie}}},\n    sessionId                : '9cb44d34977c4e2bbb39d4cfe4262f46',\n    serviceMsgUrl            : 'https://31.192.232.31:443/messaging',\n    transportWorkerUrl       : 'https://31.192.232.31:443/transport-worker.js',\n    isFirstPageLoad          : false,\n    crossDomainProxyPort     : '2053',\n    referer                  : {{{referer}}},\n    forceProxySrcForImage    : false,\n    iframeTaskScriptTemplate : {{{iframeTaskScriptTemplate}}},\n    allowMultipleWindows     : true,\n    isRecordMode             : false,\n    windowId                 : '',\n    nativeAutomation         : false,\n    disableCrossDomain       : false\n}, window);\n\n(function () {\n    \n})();\n",
+    allowMultipleWindows     : true,
+    isRecordMode             : false,
+    windowId                 : '',
+    nativeAutomation         : false,
+    disableCrossDomain       : false
+}, window);
+
+(function () {
+    
+})();
